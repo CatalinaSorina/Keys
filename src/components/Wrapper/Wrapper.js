b4e5e5d939ba473a9fd6553dc2f5040918fa1f38
CatalinaSorina <gherzan.catalina@yahoo.com>
2020-03-05 16:32:32 +0200

    added addStyle instead withStyle like the other components

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -4,53 +4,55 @@ import { wrapperDefaultStyle } from "./wrapperDefaultStyle";
 import styled from "styled-components";
 
 export const WrapperStyle = styled.div`
-  ${({ withStyle, styledComponent }) => `
-    display: ${withStyle.display};
-    flex-direction: ${withStyle.flexDirection};
-    align-items: ${withStyle.alignItems};
-    justify-content: ${withStyle.justifyContent};
-    width: ${withStyle.width};
-    height: ${withStyle.height};
-    padding: ${withStyle.padding};
-    margin: ${withStyle.margin};
-    color: ${withStyle.color};
-    border: ${withStyle.border};
-    border-color: ${withStyle.borderColor};
-    background-color: ${withStyle.backgroundColor};
-    overflow: ${withStyle.overflow};
-    cursor: ${withStyle.cursor};
+  ${({ addStyle, styledComponent }) => {
+    return `
+    display: ${addStyle.display};
+    flex-direction: ${addStyle.flexDirection};
+    align-items: ${addStyle.alignItems};
+    justify-content: ${addStyle.justifyContent};
+    width: ${addStyle.width};
+    height: ${addStyle.height};
+    padding: ${addStyle.padding};
+    margin: ${addStyle.margin};
+    color: ${addStyle.color};
+    border: ${addStyle.border};
+    border-color: ${addStyle.borderColor};
+    background-color: ${addStyle.backgroundColor};
+    overflow: ${addStyle.overflow};
+    cursor: ${addStyle.cursor};
 
     *::-webkit-scrollbar {
-      width: ${withStyle.scrollbarWidth};
+      width: ${addStyle.scrollbarWidth};
     }
 
     *::-webkit-scrollbar-track {
-      border-radius: ${withStyle.scrollbarTrack.borderRadius};
-      box-shadow: ${withStyle.scrollbarTrack.boxShadow};
-      background-color: ${withStyle.scrollbarTrack.backgroundColor};
+      border-radius: ${addStyle.scrollbarTrack.borderRadius};
+      box-shadow: ${addStyle.scrollbarTrack.boxShadow};
+      background-color: ${addStyle.scrollbarTrack.backgroundColor};
     }
 
     *::-webkit-scrollbar-thumb {
-      border-radius: ${withStyle.scrollbarThumb.borderRadius};
-      box-shadow: ${withStyle.scrollbarThumb.boxShadow};
-      background-color: ${withStyle.scrollbarThumb.backgroundColor};
-      background-image: ${withStyle.scrollbarThumb.backgroundImage}
+      border-radius: ${addStyle.scrollbarThumb.borderRadius};
+      box-shadow: ${addStyle.scrollbarThumb.boxShadow};
+      background-color: ${addStyle.scrollbarThumb.backgroundColor};
+      background-image: ${addStyle.scrollbarThumb.backgroundImage}
     }
 
     *::selection {
-      color: ${withStyle.selectColor};
-      background-color:${withStyle.selectBackgroundColor};
-      text-shadow:${withStyle.selectTextShadow};
+      color: ${addStyle.selectColor};
+      background-color:${addStyle.selectBackgroundColor};
+      text-shadow:${addStyle.selectTextShadow};
     }
 
     ${styledComponent && styledComponent}
-  `}
+  `;
+  }}
 `;
 
 const Wrapper = ({ style, styledComponent, content }) => (
   <WrapperStyle
     style={removeKeysFromStyle(style, wrapperDefaultStyle)}
-    withStyle={combineStyle(style, wrapperDefaultStyle)}
+    addStyle={combineStyle(style, wrapperDefaultStyle)}
     styledComponent={styledComponent}
   >
     {content}
