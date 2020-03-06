bf68c9285fb32f705ac61260b0d71b9326758151
CatalinaSorina <gherzan.catalina@yahoo.com>
2020-03-06 11:39:24 +0200

    testing examples of StyledComponent

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Input from "./components/Input/Input";
 import Button from "./components/Button/Button";
 import Tab from "./components/Tab/Tab";
 import Paragraphs from "./components/Paragraph/Paragraphs";
-import StyledComponent from "./components/StyledComponent/StyledComponent"
+import StyledComponent from "./components/StyledComponent/StyledComponent";
 
 const tabAsModalStyle = {
   buttonStyle: {
@@ -15,6 +15,8 @@ const tabAsModalStyle = {
   tabStyle: {
   }
 }
+
+const ButtonTest=(props)=><button {...props}>test styled button</button>
 
 const App = () => (
   <Wrapper
@@ -58,24 +60,25 @@ const App = () => (
         tabAsModal={true}
         modalCloseButtonStyle={{boxShadow:"none"}}
         content={<>
-          <div>
+          <StyledComponent
+            type="div"
+            styledComponent="background-color:green;padding:2rem;margin:1rem;"
+          >
             <Paragraphs text="Lorem ipsum dolor amet stumptown actually subway tile squid try-hard tilde biodiesel taiyaki paleo. Coloring book palo santo jean shorts green juice 90's hoodie chicharrones hammock. Chicharrones heirloom 90's polaroid ugh hella. Craft beer tousled organic tacos gastropub church-key. Post-ironic heirloom butcher unicorn pitchfork cloud bread direct trade offal kitsch fam. Lomo street art paleo blue bottle plaid poke pitchfork everyday carry master cleanse godard photo booth gastropub. Typewriter vinyl pitchfork keytar readymade, cold-pressed austin drinking vinegar chartreuse franzen banh mi gluten-free.
               Sartorial forage PBR&B, gluten-free shaman banjo man braid next level scenester. Cliche cray fingerstache selfies trust fund YOLO. Swag aesthetic shaman, semiotics woke cornhole iceland mumblecore sartorial artisan organic salvia bushwick mixtape. Tattooed salvia mustache pop-up put a bird on it.
               Godard enamel pin vape synth vaporware. Viral everyday carry portland pork belly locavore. Hella sartorial twee umami keytar, pitchfork biodiesel hashtag snackwave hot chicken gluten-free lyft poutine. Narwhal shabby chic irony kickstarter tbh, chia mixtape gentrify listicle.
               8-bit church-key seitan marfa ennui, wolf bitters. Pok pok cliche woke stumptown gluten-free vexillologist celiac slow-carb tattooed hammock enamel pin schlitz raclette four dollar toast poke. Live-edge poke actually, godard flexitarian subway tile taxidermy iceland forage craft beer PBR&B. Kale chips cronut enamel pin, tattooed chillwave plaid adaptogen tacos meditation. Subway tile disrupt artisan leggings, air plant asymmetrical 3 wolf moon gentrify paleo vice meditation celiac cardigan portland XOXO. Williamsburg health goth chillwave irony.
               Schlitz truffaut cronut, taiyaki asymmetrical heirloom pinterest leggings disrupt. Biodiesel raw denim truffaut hammock, franzen humblebrag cold-pressed food truck ennui thundercats dreamcatcher four loko forage blog cred. Cloud bread shoreditch ramps edison bulb photo booth mustache fanny pack quinoa cronut hashtag everyday carry heirloom. Blog plaid post-ironic hexagon." 
             />
-          </div>
+          </StyledComponent>
         </>}
       />
       select me
       <StyledComponent
-        componentType="button"
-        componentStyledComponent="background-color:transparent;height:3rem;border-radius:1rem;"
+        type={ButtonTest}
+        styledComponent="background-color:transparent;height:3rem;border-radius:1rem;"
         onClick={()=>console.log("Button styled working :D")}
-      >
-        test styled
-      </StyledComponent>
+      />
       <Input 
         placeholder="try me"
         styledComponent="color:red; &::placeholder {color:rgba(255, 0, 0,0.7);}"
--- src/components/StyledComponent/StyledComponent.js
+++ src/components/StyledComponent/StyledComponent.js
@@ -2,8 +2,8 @@ import React from "react";
 import styled from 'styled-components';
 
 const StyledComponent = (props) => {
-    const Component = styled(props.componentType)`
-        ${props.componentStyledComponent && props.componentStyledComponent}
+    const Component = styled(props.type)`
+        ${props.styledComponent && props.styledComponent}
     `
     return <Component {...props} />
 };
