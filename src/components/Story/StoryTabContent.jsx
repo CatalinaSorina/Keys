d1da3bc8c79a21656a2f880147c397dd8ea45b3e
CatalinaSorina <gherzan.catalina@yahoo.com>
2021-03-30 15:02:02 +0300

    solved eslint Story for TabContent

--- src/components/Story/StoryTabContent.jsx
+++ src/components/Story/StoryTabContent.jsx
@@ -1,31 +1,45 @@
-//@flow
+/* eslint-disable react/no-array-index-key */
+// @flow
 import React from 'react';
 import type { Node } from 'react';
 import Highlighter from '../Highlighter/Highlighter';
-import { getKey } from "../keys";
+import { getKey } from '../keys';
 
 type StoryTabContentProps = {
     keyName:string,
     keyProps:Array<string>,
-    examples:Array<{props:{},code:string}>,
+    examples:Array<{props:{}, code:string}>,
     columns:number,
     hidePropsTitle:boolean,
     children:Node,
 }
 
-const StoryTabContent = ({keyName,keyProps,examples,columns,hidePropsTitle,children}:StoryTabContentProps) => {
-    const Key=getKey(keyName);
+const StoryTabContent = ({
+ keyName, keyProps, examples, columns, hidePropsTitle, children,
+}:StoryTabContentProps) => {
+    const Key = getKey(keyName);
 
-    return <div>
-        <h1>Key {keyName}</h1>
+    return (
+      <div>
+        <h1>
+          Key
+          {keyName}
+        </h1>
         {!hidePropsTitle && <h3>Properties:</h3>}
-        <ul style={{columns:columns}}> {keyProps.map((prop,index)=><li key={`prop${index}`}>{prop}</li>)} </ul>
-        {examples && examples.map((example,index)=><div key={`example${index}`}>
+        <ul style={{ columns }}>
+          {' '}
+          {keyProps.map((prop, index) => <li key={`prop${index}`}>{prop}</li>)}
+          {' '}
+        </ul>
+        {examples && examples.map((example, index) => (
+          <div key={`example${index}`}>
             {example.props && <Key {...example.props} />}
             {example.code && <Highlighter code={example.code} />}
-        </div>)}
+          </div>
+))}
         {children}
-    </div>
-}
+      </div>
+);
+};
 
 export default StoryTabContent;
